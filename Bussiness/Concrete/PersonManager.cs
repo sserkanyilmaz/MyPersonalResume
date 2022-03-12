618dfa2a112ca0137282df061c731e7da1a44ea3
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-03-12 13:43:59 +0300

    Update PersonManager.cs

--- Bussiness/Concrete/PersonManager.cs
+++ Bussiness/Concrete/PersonManager.cs
@@ -1,6 +1,40 @@
-﻿namespace Bussiness.Concrete
+using Bussiness.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System.Collections.Generic;
+
+namespace Bussiness.Concrete
 {
-    public class PersonManager
+    public class PersonManager : IPersonService
     {
+        private IPersonDal _personDal;
+        public PersonManager(IPersonDal personDal)
+        {
+            _personDal = personDal;
+        }
+        public void Add(Person entity)
+        {
+            _personDal.Add(entity);
+        }
+
+        public void Delete(Person entity)
+        {
+            _personDal.Delete(entity);
+        }
+
+        public Person Get(int id)
+        {
+            return _personDal.Get(x => x.Id == id);
+        }
+
+        public List<Person> GetAll()
+        {
+            return _personDal.GetAll();
+        }
+
+        public void Update(Person entity)
+        {
+            _personDal.Update(entity);
+        }
     }
 }
