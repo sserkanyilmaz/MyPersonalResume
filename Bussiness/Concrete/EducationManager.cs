e0658758875b891c8e6d080762c6bb58ee10d628
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-03-12 13:42:17 +0300

    Update EducationManager.cs

--- Bussiness/Concrete/EducationManager.cs
+++ Bussiness/Concrete/EducationManager.cs
@@ -1,6 +1,44 @@
-﻿namespace Bussiness.Concrete
+using Bussiness.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
 {
-    public class EducationManager
+    public class EducationManager : IEducationService
     {
+        private IEducationDal _educationDal;
+        public EducationManager(IEducationDal educationDal)
+        {
+            _educationDal = educationDal;
+        }
+        public void Add(Education entity)
+        {
+            _educationDal.Add(entity);
+        }
+
+        public void Delete(Education entity)
+        {
+            _educationDal.Delete(entity);
+        }
+
+        public Education Get(int id)
+        {
+            return _educationDal.Get(x => x.Id == id);
+        }
+
+        public List<Education> GetAll()
+        {
+            return _educationDal.GetAll();
+        }
+
+        public void Update(Education entity)
+        {
+            _educationDal.Update(entity);
+        }
     }
 }
