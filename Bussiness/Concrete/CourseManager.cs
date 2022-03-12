eb76f69c94db1c39bcd08961bf42473de9062f18
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-03-12 13:41:44 +0300

    Update CourseManager.cs

--- Bussiness/Concrete/CourseManager.cs
+++ Bussiness/Concrete/CourseManager.cs
@@ -1,4 +1,8 @@
-﻿using System;
+using Bussiness.Abstract;
+using DataAccess.Abstract;
+using Entity.Abstract;
+using Entity.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,38 @@ using System.Threading.Tasks;
 
 namespace Bussiness.Concrete
 {
-    public class CourseManager
+    public class CourseManager : ICourseService
     {
+        private ICourseDal _courseDal;
+        public CourseManager(ICourseDal courseDal)
+        {
+            _courseDal = courseDal;
+        }
+        public void Add(Course entity)
+        {
+            _courseDal.Add(entity);
+        }
+
+        public void Delete(Course entity)
+        {
+            _courseDal.Delete(entity);
+        }
+
+        public Course Get(int id)
+        {
+            return _courseDal.Get(x => x.Id == id);
+        }
+
+        public List<Course> GetAll()
+        {
+            return _courseDal.GetAll();
+        }
+
+        public void Update(Course entity)
+        {
+            _courseDal.Update(entity);
+
+        }
+        
     }
 }
