6b67797ca198ddef4a3f573d7d56bd30aad8eec1
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-03-12 13:42:46 +0300

    Update ExperienceManager.cs

--- Bussiness/Concrete/ExperienceManager.cs
+++ Bussiness/Concrete/ExperienceManager.cs
@@ -1,6 +1,40 @@
-﻿namespace Bussiness.Concrete
+using Bussiness.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System.Collections.Generic;
+
+namespace Bussiness.Concrete
 {
-    public class ExperienceManager
+    public class ExperienceManager : IExperienceService
     {
+        private IExperienceDal _experienceDal;
+        public ExperienceManager(IExperienceDal experienceDal)
+        {
+            _experienceDal = experienceDal;
+        }
+        public void Add(Experience entity)
+        {
+            _experienceDal.Add(entity);
+        }
+
+        public void Delete(Experience entity)
+        {
+            _experienceDal.Delete(entity);
+        }
+
+        public Experience Get(int id)
+        {
+            return _experienceDal.Get(x => x.Id == id);
+        }
+
+        public List<Experience> GetAll()
+        {
+            return _experienceDal.GetAll();
+        }
+
+        public void Update(Experience entity)
+        {
+            _experienceDal.Update(entity);
+        }
     }
 }
