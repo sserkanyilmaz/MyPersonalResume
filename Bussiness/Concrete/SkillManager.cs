9ab4bc65991451b2612b106f1ab62abfcec081fd
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-03-12 13:44:37 +0300

    Update SkillManager.cs

--- Bussiness/Concrete/SkillManager.cs
+++ Bussiness/Concrete/SkillManager.cs
@@ -1,6 +1,40 @@
-﻿namespace Bussiness.Concrete
+using Bussiness.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System.Collections.Generic;
+
+namespace Bussiness.Concrete
 {
-    public class SkillManager
+    public class SkillManager : ISkillService
     {
+        private ISkillDal _skillDal;
+        public SkillManager(ISkillDal skillDal)
+        {
+            _skillDal = skillDal;
+        }
+        public void Add(Skill entity)
+        {
+            _skillDal.Add(entity);
+        }
+
+        public void Delete(Skill entity)
+        {
+            _skillDal.Delete(entity);
+        }
+
+        public Skill Get(int id)
+        {
+            return _skillDal.Get(x => x.Id == id);
+        }
+
+        public List<Skill> GetAll()
+        {
+            return _skillDal.GetAll();
+        }
+
+        public void Update(Skill entity)
+        {
+            _skillDal.Update(entity);
+        }
     }
 }
