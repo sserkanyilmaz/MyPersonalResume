3fa8c0b1acd50d45c96624f4de171a0b144a7738
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-03-12 13:43:31 +0300

    Update MessageManager.cs

--- Bussiness/Concrete/MessageManager.cs
+++ Bussiness/Concrete/MessageManager.cs
@@ -1,6 +1,41 @@
-﻿namespace Bussiness.Concrete
+using Bussiness.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System.Collections.Generic;
+
+namespace Bussiness.Concrete
 {
-    public class MessageManager
+    public class MessageManager : IMessageService
     {
+        private IMessageDal _messageDal;
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+        public void Add(Message entity)
+        {
+            _messageDal.Add(entity);
+        }
+
+        public void Delete(Message entity)
+        {
+            _messageDal.Delete(entity);
+        }
+
+        public Message Get(int id)
+        {
+            return _messageDal.Get(x => x.Id == id);
+
+        }
+
+        public List<Message> GetAll()
+        {
+            return _messageDal.GetAll();
+        }
+
+        public void Update(Message entity)
+        {
+            _messageDal.Update(entity);
+        }
     }
 }
